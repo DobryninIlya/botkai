c7c318f0a4b9af5c7832efc68a3a01b39bce5a2e
Илья <dobryninis@stud.kai.ru>
2020-07-05 21:42:59 +0300

    first commit

--- botkai/views.py
+++ botkai/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import json
 import traceback
 
-
+@csrf_exempt
 def index(request):
     try:
         pass
