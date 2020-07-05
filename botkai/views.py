1c20ce24727ee57d386881e36419f85703a977be
Илья <dobryninis@stud.kai.ru>
2020-07-05 20:41:04 +0300

    first commit

--- botkai/views.py
+++ botkai/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+import json
 
 def index(request):
-    #print(request.data)
-    #print(request.params)
-    return "<!DOCTYPE><html><p6>КАКАЯ ТА НАДПИСЬ</p6></html>"
+    print(json.loads(request.data))
+    print((json.loads(request.data)).keys())
+    return "ЩЛ"
