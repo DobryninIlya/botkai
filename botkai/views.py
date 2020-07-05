003821e04f9c2099b6cc27cea563429f737431c6
Илья <dobryninis@stud.kai.ru>
2020-07-05 20:46:23 +0300

    first commit

--- botkai/views.py
+++ botkai/views.py
@@ -4,8 +4,8 @@ import traceback
 
 def index(request):
     try:
-        print(json.loads(request.data))
-        print((json.loads(request.data)).keys())
+        print(json.loads(request.body))
+        print((json.loads(request.body)).keys())
     except:
         print('Ошибка:\n', traceback.format_exc())
     return "ЩЛ"
