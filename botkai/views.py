a854f70c4154ee344302ce261c2afba0180cb02b
Илья <dobryninis@stud.kai.ru>
2020-07-05 21:05:47 +0300

    first commit

--- botkai/views.py
+++ botkai/views.py
@@ -5,6 +5,8 @@ import traceback
 def index(request):
     try:
         print(request.body)
+        print(request.method)
+        print(request.META)
     except:
         print('Ошибка:\n', traceback.format_exc())
     return "ЩЛ"
