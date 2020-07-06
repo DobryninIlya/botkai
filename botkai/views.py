576961c5d3ab15ebdb6ecb560dc41230924cf0f1
Илья <dobryninis@stud.kai.ru>
2020-07-06 14:55:40 +0300

    first commit

--- botkai/views.py
+++ botkai/views.py
@@ -28,9 +28,9 @@ def index(request):
             result = "Неа."
         
         else:
-            result = eval("botkai." + body["type"]+".func")
+            result = eval("botkai.events." + body["type"]+".func")
             #result = eval("/callbackevents/confirmation.index")
-            #print(os.path.abspath(__file__))
+            #rint(os.path.abspath(__file__))
             #result = func()
 
 
