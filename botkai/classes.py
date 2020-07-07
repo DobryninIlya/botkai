31e47402877f2f2d6c8b4cc635761f69e77eafb0
Илья <dobryninis@stud.kai.ru>
2020-07-07 18:55:57 +0300

    first commit

--- botkai/classes.py
+++ botkai/classes.py
@@ -1,9 +1,7 @@
 import psycopg2
 
-class connections:
-    connection = psycopg2.connect(dbname='dfdn09mdk3r1gr', user='olkywigpsefwye', password='6f73707c0610067f60ed525f472fcbc34e3af291dbc21e6bec1d6d3ed89c94b9', host='ec2-54-246-121-32.eu-west-1.compute.amazonaws.com')
-    cursor = ''
-
+class connections(self):
     def __init__(self):
-        cursor = connection.cursor()
+        self.connection = psycopg2.connect(dbname='dfdn09mdk3r1gr', user='olkywigpsefwye', password='6f73707c0610067f60ed525f472fcbc34e3af291dbc21e6bec1d6d3ed89c94b9', host='ec2-54-246-121-32.eu-west-1.compute.amazonaws.com')
+        self.cursor = connection.cursor()
 
