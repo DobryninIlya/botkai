447f36bdacea621fac44a796b504180558e07892
Илья <dobryninis@stud.kai.ru>
2020-07-08 20:59:02 +0300

    t

--- botkai/events/message_new.py
+++ botkai/events/message_new.py
@@ -44,7 +44,7 @@ def message_new(request):
     try:
         global message_params
         message_params = json.loads(request.body)
-        MessageSettings.update()
+        classes.MessageSettings.update()
         if IsRegistred():
             print("Зарегистрирован")
             UserParams.update(int(message_params["object"]["message"]["from_id"]))
