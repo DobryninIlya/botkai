48cf71941bd8701b97d79221cd2c6d32bd07a3a9
Илья <dobryninis@stud.kai.ru>
2020-07-08 14:42:46 +0300

    t

--- botkai/classes.py
+++ botkai/classes.py
@@ -5,6 +5,7 @@ import sqlite3
 class connections:
     def __init__(self):
         self.connection = psycopg2.connect(dbname='dfdn09mdk3r1gr', user='olkywigpsefwye', password='6f73707c0610067f60ed525f472fcbc34e3af291dbc21e6bec1d6d3ed89c94b9', host='ec2-54-246-121-32.eu-west-1.compute.amazonaws.com')
+        self.connection.autocommit=True
         self.cursor = self.connection.cursor()
         self.conn = sqlite3.connect("bot.db")
         self.cursorR = self.conn.cursor()
