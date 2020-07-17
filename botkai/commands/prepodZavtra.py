294be45e4b670e674d609c29fb6b25ce31d8662d
Илья <dobryninis@stud.kai.ru>
2020-07-17 17:02:00 +0300

    edited

--- botkai/commands/prepodZavtra.py
+++ botkai/commands/prepodZavtra.py
@@ -1,5 +1,5 @@
 from .. import classes as command_class
-import ..keyboards
+from .. import keyboards
 from ..classes import vk, MessageSettings, UserParams, conn, cursorR
 import random
 import requests
