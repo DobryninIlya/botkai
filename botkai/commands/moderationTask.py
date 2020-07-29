90a5163589c3b82afcb09d1e75a724c0fa526c4f
Илья <dobryninis@stud.kai.ru>
2020-07-29 16:32:57 +0300

    d

--- botkai/commands/moderationTask.py
+++ botkai/commands/moderationTask.py
@@ -1,5 +1,5 @@
 from .. import classes as command_class
-from ..keyboards import GetModerAdvButton
+from ..keyboards import GetModerTaskButton
 from ..classes import vk, MessageSettings, UserParams, connection, cursor
 import random
 import traceback
