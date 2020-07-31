6f1bcb44519f3d5e00b1231985737246eb09e1cf
Илья <dobryninis@stud.kai.ru>
2020-07-31 21:09:58 +0300

    t

--- botkai/commands/starosta_moder_task_next.py
+++ botkai/commands/starosta_moder_task_next.py
@@ -34,6 +34,15 @@ def info():
         prev_id_task = 1
         id = -1 
         for row in curs:
+            if val_id == 1:
+                id =  (int)(row[0])
+                task = "❗зᴀдᴀниᴇ❗\n"
+                task += str(row[4])
+                idvk = "@id" + str(row[2])
+                task += "\n" + idvk + " (Автор) | ID: " + str(row[0])
+                att = str(row[5])
+                first = second = False
+                
             if first :
                 prev_id_task = int(row[0])
                 print("prev id ", prev_id_task)
