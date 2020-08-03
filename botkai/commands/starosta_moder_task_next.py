7ad11fdb298c09caa7dde840be77fecd93868fcf
Илья <dobryninis@stud.kai.ru>
2020-08-03 17:11:03 +0300

    t

--- botkai/commands/starosta_moder_task_next.py
+++ botkai/commands/starosta_moder_task_next.py
@@ -15,7 +15,7 @@ def info():
         val_id = MessageSettings.payload["id"]
         pos_id = MessageSettings.payload["pos_id"]
         #message_id = MessageSettings.payload["msg_id"]
-        sql = "SELECT * FROM Task WHERE groupid = {} LIMIT 3 OFFSET {}".format(groupId, 0 if int(pos_id)-1 < 0 else int(pos_id)-1)
+        sql = "SELECT * FROM Task WHERE groupid = {} LIMIT 3 OFFSET {}".format(groupId, 0 if int(pos_id) < 0 else int(pos_id))
         print(sql)
         cursor.execute(sql)
         task = ""
