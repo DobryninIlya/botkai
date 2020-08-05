98931e64305063e8dd8fbf7a5fe60dd0a0f2b71f
Илья <dobryninis@stud.kai.ru>
2020-08-05 17:34:27 +0300

    alice

--- aliceskill/views.py
+++ aliceskill/views.py
@@ -5,11 +5,36 @@ import json
 import logging
 from django.views.decorators.csrf import csrf_exempt
 from pprint import pprint
+import os
+
+
+import random
+import datetime
+import json
+import requests
+import traceback
+
 
 sessionStorage = {}
 commands = ['расписание']
 
 logging.basicConfig(level=logging.DEBUG)
+
+
+class connections:
+    def __init__(self):
+        self.connection = psycopg2.connect(dbname=os.getenv('DB_NAME'), user= os.getenv('DB_USER'), password= os.getenv('DB_PASSWORD'), host= os.getenv('DB_HOST'))
+        self.connection.autocommit=True
+        self.cursor = self.connection.cursor()
+        self.conn = sqlite3.connect("bot.db")
+        self.cursorR = self.conn.cursor()
+
+connect = connections()
+cursor = connect.cursor
+cursorR = connect.cursorR
+connection = connect.connection
+conn = connect.conn
+
 
 @csrf_exempt
 def main(request):
@@ -50,7 +75,119 @@ def handle_dialog(body, request, response):
                 print(group_values)
             if not group_values:
                 response["response"]["text"] = "Повтори все тоже самое, но с номером группы"
-            response["response"]["text"] = command + " " + group_values + " день " + str(day)
+            #response["response"]["text"] = command + " " + group_values + " день " + str(day)
+            response["response"]["text"] = info(int(group_values), )
+
             return
         else:
             response["response"]["text"] = "Я не распознал твою команду. Повтори, пожалуйста, что ты хочешь получить и номер группы."
+
+
+today = datetime.date.today()
+chetn = 1
+BASE_URL = 'https://kai.ru/raspisanie' 
+frazi = ["Можно сходить в кино 😚", "Можно почитать 😚", "Можно прогуляться в лесу 😚", "Можно распланировать дела на неделю 😚", "Можно заняться спортом, например. 😚", "Можно вспомнить строчки гимна КАИ 😚", "Можно заняться чем то интересным 😚", "Можно встретиться с друзьями 😚"]
+def info(group, day):
+    today = datetime.date.today()
+    day = day if day.isdigit() else 0
+    date = str(datetime.date(today.year, today.month, today.day)  + datetime.timedelta(days=1))
+    return showTimetable(group, day)
+
+
+def showTimetable(groupId, tomorrow=0):
+    try:
+        isNormal, response = getResponse(groupId)
+        if not isNormal:
+            return response
+        chetn = UserParams.getChetn()
+        today = datetime.date.today() + datetime.timedelta(days=tomorrow)
+
+
+        if len(response) == 0:
+            return "\n&#10060;\tРасписание еще не доступно.&#10060;"
+        
+        response = response[str(datetime.date(today.year, today.month, today.day).isoweekday())]
+        result = ''
+        now = datetime.datetime.now() + datetime.timedelta(days=tomorrow)
+        month = now.month
+        if month < 10:
+            month = "0" + str(month)
+        day = str(now.day) + "." + str(month)
+        for elem in response:
+            dateinstr = (str((elem["dayDate"]).rstrip())).find(day)
+            print(dateinstr)
+            if (elem["dayDate"]).rstrip()=="чет" and ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
+                result += str(chr(10148)) + elem["dayDate"][:3] + " " + " &#8987;" + elem["dayTime"][:5] +  " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() +' зд.\n'
+            elif (elem["dayDate"]).rstrip()=="неч" and  not ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
+                result += str(chr(10148)) + elem["dayDate"][:3] + " " + " &#8987;" + elem["dayTime"][:5] + " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() +' зд.\n'
+            elif (elem["dayDate"]).rstrip()=="неч/чет" and  not ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
+                result += str(chr(10148))  + " 1&#8419;гр. " + " &#8987;" + elem["dayTime"][:5] + " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() +' зд.\n'
+            elif (elem["dayDate"]).rstrip()=="неч/чет" and  ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
+                result += str(chr(10148))  + " 2&#8419;гр. " + " &#8987;" + elem["dayTime"][:5] + " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() +' зд.\n'
+            elif (elem["dayDate"]).rstrip()=="чет/неч" and  ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
+                result += str(chr(10148))  + " 1&#8419;гр. " + " &#8987;" + elem["dayTime"][:5] + " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() +' зд.\n'
+            elif (elem["dayDate"]).rstrip()=="чет/неч" and  not ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
+                result += str(chr(10148))  + " 2&#8419;гр. " + " &#8987;" + elem["dayTime"][:5] + " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() +' зд.\n'
+            elif dateinstr != -1:
+                result += str(chr(10148)) + str(day) + " " + " &#8987;" + elem["dayTime"][:5] + " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() + ' зд.\n'
+            elif not ((elem["dayDate"]).rstrip()=="чет") and not ((elem["dayDate"]).rstrip()=="неч"):
+                result += str(chr(10148)) + elem["dayDate"].rstrip() + " " + " &#8987;" + elem["dayTime"][:5] + " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() + ' зд.\n'
+        return result
+    except ConnectionError as err:
+        return "&#9888;Ошибка подключения к серверу типа ConnectionError. Вероятно, сервера КАИ были выведены из строя.&#9888;"
+    except requests.exceptions.Timeout as err:
+        return "&#9888;Ошибка подключения к серверу типа Timeout. Вероятно, сервера КАИ перегружены.&#9888;"
+    except KeyError as err:
+        return False
+    except Exception as E:
+        print('Ошибка:\n', traceback.format_exc())
+
+        return ""
+    
+
+
+def getResponse(groupId):
+    
+    sql = "SELECT * FROM saved_timetable WHERE groupp = {}".format(groupId)
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if result == None:
+        try:
+            
+            response = requests.post( BASE_URL, data = "groupId=" + str(groupId), headers = {'Content-Type': "application/x-www-form-urlencoded"}, params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 3)
+        except ConnectionError as err:
+            return False, "&#9888;Ошибка подключения к серверу типа ConnectionError. Вероятно, сервера КАИ были выведены из строя.&#9888;"
+        except requests.exceptions.Timeout as err:
+            return False, "&#9888;Ошибка подключения к серверу типа Timeout. Вероятно, сервера КАИ перегружены.&#9888;"
+        except:
+            return False, ""
+        sql = "INSERT INTO saved_timetable VALUES ({}, '{}', '{}')".format(groupId, datetime.date.today(), json.dumps(response.json()))
+        cursor.execute(sql)
+        connection.commit()
+        return True, response.json()
+    else:
+        date_update = result[1]
+        timetable = result[2]
+        if date_update + datetime.timedelta(days=4) >= today:
+            try:
+                raise Exception
+                response = requests.post( BASE_URL, data = "groupId=" + str(groupId), headers = {'Content-Type': "application/x-www-form-urlencoded"}, params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 3)
+                sql = "UPDATE saved_timetable SET shedule = '{}', date_update = '{}' WHERE groupp = {}".format(json.dumps(response.json()), datetime.date.today(), groupId)
+                cursor.execute(sql)
+                connection.commit()
+                return True, response.json()
+            except:
+                sql = "SELECT shedule FROM saved_timetable WHERE groupp = {}".format(groupId)
+                cursor.execute(sql)
+                result = cursor.fetchone()[0]
+                return True, json.loads(result)
+        else:
+            sql = "SELECT shedule FROM saved_timetable WHERE groupp = {}".format(groupId)
+            cursor.execute(sql)
+            result = cursor.fetchone()[0]
+            return True, json.loads(result)
+    
+    
+
+
+    return 
