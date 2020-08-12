bb1b75b88308e91a9356e81d10670d2113d8b12b
Илья <dobryninis@stud.kai.ru>
2020-08-12 13:32:12 +0300

    shed test

--- shed.py
+++ shed.py
@@ -15,7 +15,7 @@ def func():
 def scheduled_job():
     print('This job is run every weekday at 5pm.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10, minute = 27)
+#@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10, minute = 27)
 def func():
     print("working sheduler")
     distribution()
