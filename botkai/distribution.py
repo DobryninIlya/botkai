2bd13cae5a01e8ec3300fd195832c9a0de1eea89
Илья <dobryninis@stud.kai.ru>
2020-08-12 12:44:16 +0300

    shed test

--- botkai/distribution.py
+++ botkai/distribution.py
@@ -185,7 +185,7 @@ keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
 keyboard = str(keyboard.decode('utf-8'))
 
 @sched.scheduled_job('interval', hours=3)
-def main(request):
+def main(request = None):
 
 
     distrList = []
