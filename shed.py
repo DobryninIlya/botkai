d67c95561c395cff59cf5bbaf5da297f9341c47b
Илья <dobryninis@stud.kai.ru>
2020-08-14 21:43:45 +0300

    groups

--- shed.py
+++ shed.py
@@ -32,7 +32,7 @@ def getGroupsResponse():
     try:
         response = requests.post( BASE_URL + "?p_p_id=pubStudentSchedule_WAR_publicStudentSchedule10&p_p_lifecycle=2&p_p_resource_id=getGroupsURL", 
         headers = {'Content-Type': "application/x-www-form-urlencoded"}, 
-        params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 5 )
+        params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 60 )
         response = response.json()
         print("INSERT INTO saved_timetable VALUES (1, {},\"{}\")".format(datetime.date.today(), json.dumps(response)))
         # cursor.execute("INSERT INTO saved_timetable VALUES (1, \'{}\',\"{}\")".format(response, datetime.date.today())
