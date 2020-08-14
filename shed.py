5cfc3fce16f972a211f70d5308bf829cc4e7334a
Илья <dobryninis@stud.kai.ru>
2020-08-14 21:15:53 +0300

    groups

--- shed.py
+++ shed.py
@@ -34,7 +34,7 @@ def getGroupsResponse():
         headers = {'Content-Type': "application/x-www-form-urlencoded"}, 
         params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 5 )
         response = response.json()
-        cursor.execute("INSERT INTO saved_timetable VALUES (1, {},)".format(datetime.date.today(), json.dumps(response)))
+        cursor.execute("INSERT INTO saved_timetable VALUES (1, {},{})".format(datetime.date.today(), json.dumps(response)))
         connection.commit()
         sched.shutdown()
     except:
