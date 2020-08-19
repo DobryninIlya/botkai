d2739e9665bd28f27d9c9e13bfa22bb3c0f6fb3c
Илья <dobryninis@stud.kai.ru>
2020-08-19 15:52:02 +0300

    alice

--- aliceskill/views.py
+++ aliceskill/views.py
@@ -71,10 +71,10 @@ def showGroupId(groupNumber):
         group = getGroupsResponse(groupNumber)
         if group:
             return group
-        vk.method("messages.send",
-                {"peer_id": id, "message": "&#9888;Ошибка подключения к серверам.&#9888; \n Вероятно, на стороне kai.ru произошел сбой. Вам необходимо продолжить регистрацию (ввод номера группы) как только сайт kai.ru станет доступным.", "random_id": random.randint(1, 2147483647)})
-        vk.method("messages.send",
-                {"peer_id": id, "message": "test" , "sticker_id" : 18486 , "random_id": random.randint(1, 2147483647)})
+        # vk.method("messages.send",
+        #         {"peer_id": id, "message": "&#9888;Ошибка подключения к серверам.&#9888; \n Вероятно, на стороне kai.ru произошел сбой. Вам необходимо продолжить регистрацию (ввод номера группы) как только сайт kai.ru станет доступным.", "random_id": random.randint(1, 2147483647)})
+        # vk.method("messages.send",
+        #         {"peer_id": id, "message": "test" , "sticker_id" : 18486 , "random_id": random.randint(1, 2147483647)})
         return False
     except:
         print('Ошибка:\n', traceback.format_exc())
