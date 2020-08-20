64c367d563952875c2cc93cc26b1c8cfac3c6555
Илья <dobryninis@stud.kai.ru>
2020-08-20 22:59:58 +0300

    alice

---
+++ botkai/commands/starosta_distr_info.py
@@ -0,0 +1,27 @@
+from .. import classes as command_class
+import random
+from ..keyboards import make_distr
+from ..classes import vk, MessageSettings, conn, cursorR
+import traceback
+
+def info():
+
+    vk.method("messages.send",
+        {"peer_id": MessageSettings.id, "message": """Ты можешь разослать любое сообщение всей своей группе. Внимание, сообщение показывается ВСЕМ участникам группы.""","keyboard": make_distr, "random_id": random.randint(1, 2147483647)})
+
+    return "ok"
+
+
+
+
+
+command = command_class.Command()
+
+
+
+
+command.keys = []
+command.desciption = ''
+command.process = info
+command.payload = "starosta_distr_info"
+command.admlevel = 2
