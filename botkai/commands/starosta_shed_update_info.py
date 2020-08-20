e2a65cb71c7962e8c55d132d4f1730850530dfe5
Илья <dobryninis@stud.kai.ru>
2020-08-20 22:59:51 +0300

    alice

--- botkai/commands/starosta_shed_update_info.py
+++ botkai/commands/starosta_shed_update_info.py
@@ -1,13 +1,15 @@
 from .. import classes as command_class
 import random
-from ..keyboards import make_distr
+from ..keyboards import shed_update
 from ..classes import vk, MessageSettings, conn, cursorR
 import traceback
 
 def info():
 
     vk.method("messages.send",
-        {"peer_id": MessageSettings.id, "message": """Ты можешь разослать любое сообщение всем участникам группы""","keyboard": make_distr, "random_id": random.randint(1, 2147483647)})
+        {"peer_id": MessageSettings.id, "message": """Расписание хранится в буфере на сервере и автоматически обновляется с переодичностью в несколько дней. 
+        Когда происходит запрос расписания, оно берется именно из буфера и может получиться так, что расписание окажется не актуальным.
+        Эта функция служит для принудительного обновления расписания в Базе Данных сервера.""","keyboard": shed_update, "random_id": random.randint(1, 2147483647)})
 
     return "ok"
 
@@ -23,5 +25,5 @@ command = command_class.Command()
 command.keys = []
 command.desciption = ''
 command.process = info
-command.payload = "starosta_distr_info"
+command.payload = "starosta_shed_update_info"
 command.admlevel = 2
