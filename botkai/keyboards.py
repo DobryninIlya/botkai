eed14c45e668afe7cd6f03c8f9610d86e8c2e90b
Илья <dobryninis@stud.kai.ru>
2020-08-24 15:50:57 +0300

    distribution all admin

--- botkai/keyboards.py
+++ botkai/keyboards.py
@@ -353,7 +353,6 @@ def getMainKeyboard(role):
             }
         keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
         keyboard = str(keyboard.decode('utf-8'))
-    print(keyboard)
     return keyboard
 
 
