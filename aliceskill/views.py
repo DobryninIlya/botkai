7f1910d6b2fc1a0882477b1bd8341654a0a09cf6
Илья <dobryninis@stud.kai.ru>
2020-10-25 20:33:05 +0300

    t

--- aliceskill/views.py
+++ aliceskill/views.py
@@ -283,9 +283,10 @@ def getResponse(groupId):
             timetable = result[2]
             if date_update + datetime.timedelta(days=4) >= today:
                 try:
+
                     response = requests.post( BASE_URL, data = "groupId=" + str(groupId), headers = {'Content-Type': "application/x-www-form-urlencoded"}, params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 1)
                     sql = "UPDATE saved_timetable SET shedule = '{}', date_update = '{}' WHERE groupp = {}".format(json.dumps(response.json()), datetime.date.today(), groupId)
-                    cursor.execute(sql)
+                    # cursor.execute(sql) # ЗАКОМЕНЧЕНО СПЕЦИАЛЬНО НА ВРЕМЯ
                     connection.commit()
                     return True, response.json()
                 except:
