4e36cdc00b6ce7646dea8e8ef1dfeeec81653d5b
IlyaDobrynin <mr.woodysimpson@gmail.com>
2020-12-08 20:59:55 +0300

    hotfix update shed

--- botkai/commands/zavtra.py
+++ botkai/commands/zavtra.py
@@ -139,6 +139,7 @@ def getResponse(groupId):
         if date_update + datetime.timedelta(days=2) < today:
             try:
                 response = requests.post( BASE_URL, data = "groupId=" + str(groupId), headers = {'Content-Type': "application/x-www-form-urlencoded"}, params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 3)
+                assert json.dumps(response.json()), "Расписание имеет некорректную форму"
                 sql = "UPDATE saved_timetable SET shedule = '{}', date_update = '{}' WHERE groupp = {}".format(json.dumps(response.json()), datetime.date.today(), groupId)
                 cursor.execute(sql)
                 connection.commit()
