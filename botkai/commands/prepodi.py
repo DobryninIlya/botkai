fec40ffe78b476592b474c0eebd1ab574262c107
IlyaDobrynin <mr.woodysimpson@gmail.com>
2021-01-26 14:54:47 +0300

    t

--- botkai/commands/prepodi.py
+++ botkai/commands/prepodi.py
@@ -67,33 +67,44 @@ class Prepodi:
 
 
 def getResponse(groupId):
-    today = datetime.date.today()
+    if UserParams.own_shed:
+        groupId = MessageSettings.getId() + 1_000_000_000
+        return get_own_shed(groupId)
 
     sql = "SELECT * FROM saved_timetable WHERE groupp = {}".format(groupId)
     cursor.execute(sql)
     result = cursor.fetchone()
     if result == None:
         try:
-            
-            response = requests.post( BASE_URL, data = "groupId=" + str(groupId), headers = {'Content-Type': "application/x-www-form-urlencoded"}, params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 3)
+
+            response = requests.post(BASE_URL, data="groupId=" + str(groupId),
+                                     headers={'Content-Type': "application/x-www-form-urlencoded"},
+                                     params={"p_p_id": "pubStudentSchedule_WAR_publicStudentSchedule10",
+                                             "p_p_lifecycle": "2", "p_p_resource_id": "schedule"}, timeout=3)
+            sql = "INSERT INTO saved_timetable VALUES ({}, '{}', '{}')".format(groupId, datetime.date.today(),
+                                                                               json.dumps(response.json()))
+            cursor.execute(sql)
+            connection.commit()
+            return True, response.json()
         except ConnectionError as err:
             return False, "&#9888;Ошибка подключения к серверу типа ConnectionError. Вероятно, сервера КАИ были выведены из строя.&#9888;"
         except requests.exceptions.Timeout as err:
             return False, "&#9888;Ошибка подключения к серверу типа Timeout. Вероятно, сервера КАИ перегружены.&#9888;"
         except:
             return False, ""
-        sql = "INSERT INTO saved_timetable VALUES ({}, '{}', '{}')".format(groupId, datetime.date.today(), json.dumps(response.json()))
-        cursor.execute(sql)
-        connection.commit()
-        return True, response.json()
+
     else:
         date_update = result[1]
         timetable = result[2]
-        if date_update + datetime.timedelta(days=4) >= today:
+        if date_update + datetime.timedelta(days=2) < today:
             try:
-                raise Exception
-                response = requests.post( BASE_URL, data = "groupId=" + str(groupId), headers = {'Content-Type': "application/x-www-form-urlencoded"}, params = {"p_p_id":"pubStudentSchedule_WAR_publicStudentSchedule10","p_p_lifecycle":"2","p_p_resource_id":"schedule"}, timeout = 3)
-                sql = "UPDATE saved_timetable SET shedule = '{}', date_update = '{}' WHERE groupp = {}".format(json.dumps(response.json()), datetime.date.today(), groupId)
+                response = requests.post(BASE_URL, data="groupId=" + str(groupId),
+                                         headers={'Content-Type': "application/x-www-form-urlencoded"},
+                                         params={"p_p_id": "pubStudentSchedule_WAR_publicStudentSchedule10",
+                                                 "p_p_lifecycle": "2", "p_p_resource_id": "schedule"}, timeout=3)
+                assert json.dumps(response.json()), "Расписание имеет некорректную форму"
+                sql = "UPDATE saved_timetable SET shedule = '{}', date_update = '{}' WHERE groupp = {}".format(
+                    json.dumps(response.json()), datetime.date.today(), groupId)
                 cursor.execute(sql)
                 connection.commit()
                 return True, response.json()
@@ -106,12 +117,39 @@ def getResponse(groupId):
             sql = "SELECT shedule FROM saved_timetable WHERE groupp = {}".format(groupId)
             cursor.execute(sql)
             result = cursor.fetchone()[0]
+            if len(result) < 10:
+                try:
+                    response = requests.post(BASE_URL, data="groupId=" + str(groupId),
+                                             headers={'Content-Type': "application/x-www-form-urlencoded"},
+                                             params={"p_p_id": "pubStudentSchedule_WAR_publicStudentSchedule10",
+                                                     "p_p_lifecycle": "2", "p_p_resource_id": "schedule"}, timeout=3)
+                    assert json.dumps(response.json()), "Расписание имеет некорректную форму"
+                    sql = "UPDATE saved_timetable SET shedule = '{}', date_update = '{}' WHERE groupp = {}".format(
+                        json.dumps(response.json()), datetime.date.today(), groupId)
+                    cursor.execute(sql)
+                    connection.commit()
+                    return True, response.json()
+                except:
+                    return True, ""
             return True, json.loads(result)
-    
-    
+
+    return
 
 
-    return 
+def get_own_shed(groupId):
+    try:
+        sql = "SELECT shedule FROM saved_timetable WHERE groupp = {}".format(groupId)
+        cursor.execute(sql)
+        result = cursor.fetchone()[0]
+        print(result)
+        if not result:
+            UserParams.own_shed = 0
+            info()
+        else:
+            return True, json.loads(result)
+    except:
+        return False, "\n&#9888; Вы выбрали отображать собственное расписание, загруженное из Excele таблицы. В базе отсутствует такое расписание. Чтобы это исправить - либо загрузите расписание, либо смените в профиле способ получения расписания на 'Использовать расписание группы' &#9888;\n"
+
 
 def GetPrepodList():
     prepodList.clear()
