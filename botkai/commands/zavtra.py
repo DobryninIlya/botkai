4daf485b06194f0426b6bb16f79f7d5e4b8ca95a
IlyaDobrynin <mr.woodysimpson@gmail.com>
2021-01-26 11:24:01 +0300

    t

--- botkai/commands/zavtra.py
+++ botkai/commands/zavtra.py
@@ -115,7 +115,7 @@ def showTimetable(groupId, tomorrow=0):
 
 def getResponse(groupId):
     if UserParams.own_shed:
-        groupId = groupId + 1_000_000_000
+        groupId = MessageSettings.getId() + 1_000_000_000
         print(groupId)
     sql = "SELECT * FROM saved_timetable WHERE groupp = {}".format(groupId)
     cursor.execute(sql)
