783bab5953ac3c4cca543566e84a9b78a72e43f7
= <mr.woodysimpson@gmail.com>
2021-02-08 12:11:07 +0300

    pg

--- botkaiD/settings.py
+++ botkaiD/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, "static"), '/your specific directory/',
+)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
