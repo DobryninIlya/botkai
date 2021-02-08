abba20ff15abd9f1e17c71c52c1a6d4fd2a3ff21
= <mr.woodysimpson@gmail.com>
2021-02-08 14:03:25 +0300

    pg

--- botkaiD/settings.py
+++ botkaiD/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, "static"), '/botkai/static/botkai',
+os.path.join(BASE_DIR, "static"), '/botkai/static',
 )
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
