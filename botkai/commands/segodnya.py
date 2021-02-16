85b55076845e3d7998111713f27a475fc13426a9
= <mr.woodysimpson@gmail.com>
2021-02-16 11:32:55 +0300

    Fixed console output

--- botkai/commands/segodnya.py
+++ botkai/commands/segodnya.py
@@ -77,7 +77,7 @@ def showTimetable(groupId, tomorrow=0):
                if not user_potok:
                    continue 
 
-            print(dateinstr)
+            # print(dateinstr)
             if (elem["dayDate"]).rstrip()=="чет" and ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
                 result += str(chr(10148)) + elem["dayDate"][:3] + " " + " &#8987;" + elem["dayTime"][:5] +  " " + elem["disciplType"][:4] + " " + elem["disciplName"] + " " + (elem["audNum"]).rstrip() + " " + (elem["buildNum"]).rstrip() +' зд.\n'
             elif (elem["dayDate"]).rstrip()=="неч" and  not ((datetime.date(today.year, today.month, today.day).isocalendar()[1] + chetn) % 2 == 0):
