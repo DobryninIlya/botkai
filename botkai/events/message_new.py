455b777708e3ef813fcbb62e5b95f4888408f179
= <mr.woodysimpson@gmail.com>
2021-03-09 19:17:52 +0300

    affiliate integration

--- botkai/events/message_new.py
+++ botkai/events/message_new.py
@@ -324,7 +324,7 @@ def IsRegistred():
                             conn.commit()
                             UserParams.update(int(MessageSettings.id))
                             vk.method("messages.send",
-                                {"peer_id": id, "message": "Ваше расписание отстутствует на сайте КАИ, однако вы"
+                                {"peer_id": id, "message": "Ваше расписание может отсутствовать на сайте КАИ, однако вы"
                                        " можете добавить самостоятельно. Следуйте инструкциям! \n &#128295; Раздел в стадии тестирования :) &#128295;"
                                                            ,"keyboard" : keyboards.getMainKeyboard(6), "random_id": random.randint(1, 2147483647)})
                             vk.method("messages.send",
