aa7d2374ee74c59a4b190f25118b940c6c0da143
= <mr.woodysimpson@gmail.com>
2021-03-10 16:19:50 +0300

    t\

--- botkai/commands/export_calendar.py
+++ botkai/commands/export_calendar.py
@@ -121,7 +121,7 @@ def makeFile(week, group):
                 current_date = current_date + datetime.timedelta(days=1)
                 continue
         current_week += 1
-        chetn += 1
+        current_date += datetime.timedelta(days=7)
     with open('{}.ics'.format(group), 'w') as f:
         f.write(str(c))
 
