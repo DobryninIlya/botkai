911f0bc69a017768a9e9f5cc884a494d33db8de4
= <mr.woodysimpson@gmail.com>
2021-03-14 22:29:23 +0300

    t

--- botkai/events/message_new.py
+++ botkai/events/message_new.py
@@ -376,7 +376,7 @@ def IsRegistred():
                     response = requests.post(
                         'https://kai.ru/for-staff/raspisanie?p_p_id=pubLecturerSchedule_WAR_publicLecturerSchedule10&p_p'
                         '_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=getLecturersURL&p_p_cacheability='
-                        'cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&query='+str(body.encode('utf-8')))
+                        'cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&query='+body.encode('utf-8'))
                     print(response.json())
 
                     if not len(response.json()):
