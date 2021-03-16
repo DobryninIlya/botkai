63515dfa630df4fadb05c7e2b06e694f569d9a13
= <mr.woodysimpson@gmail.com>
2021-03-16 13:54:26 +0300

    t

--- botkaiD/wsgi.py
+++ botkaiD/wsgi.py
@@ -7,17 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'botkaiD.settings')
-#
-# application = get_wsgi_application()
-
+import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'botkaiD.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+#
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
