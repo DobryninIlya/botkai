0274131b69a1f4a2ae41bc4351a6fd830d21665c
= <mr.woodysimpson@gmail.com>
2021-03-18 17:13:49 +0300

    t

--- botkaiD/settings.py
+++ botkaiD/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'liu2agg0tn5b5#j8&4q#9+m9oyt()3(6%w(-82ahmtc!hu1_so'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
