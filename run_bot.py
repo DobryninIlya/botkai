79cc5bff00bc215b01cdbca101237f67d543f1b6
= <mr.woodysimpson@gmail.com>
2023-02-04 09:43:46 +0300

    main error display fixed

--- run_bot.py
+++ run_bot.py
@@ -14,22 +14,22 @@ def run():
 
     bot = Bot(os.getenv('VK_TOKEN'), 7)
     try:
-        sys.stdout.write('bot has been started')
+        sys.stdout.write('bot has been started\n')
         loop.create_task(bot.start())
         loop.run_forever()
         sys.stdout.flush()
     except KeyboardInterrupt:
-        sys.stdout.write("\nstopping", datetime.datetime.now())
+        sys.stdout.write("\nstopping\n", datetime.datetime.now())
         loop.run_until_complete(bot.stop())
-        sys.stdout.write('bot has been stopped', datetime.datetime.now())
+        sys.stdout.write('bot has been stopped\n', datetime.datetime.now())
         sys.stdout.flush()
     except:
-        sys.stdout.write('Ошибка:\n', traceback.format_exc())
+        sys.stdout.write('\nОшибка:\n', traceback.format_exc())
         sys.stdout.flush()
 
 
 def signal_handler(signal, frame):
-    sys.stdout.write('programm is down ', signal)
+    sys.stdout.write('\nprogramm is down \n', signal)
     sys.stdout.flush()
     cursor.close()
     cursorR.close()
@@ -41,16 +41,16 @@ signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == '__main__':
     try:
-        sys.stdout.write('programm is running')
+        sys.stdout.write('programm is running\n')
         sys.stdout.flush()
         run()
     except:
-        sys.stdout.write('Ошибка (глобальная):\n', traceback.format_exc())
+        sys.stdout.write('\nОшибка (глобальная):\n', traceback.format_exc())
         sys.stdout.flush()
     finally:
         cursor.close()
         cursorR.close()
         conn.close()
         connection.close()
-        sys.stdout.write('programm is crashed')
+        sys.stdout.write('\nprogramm is crashed\n')
         sys.stdout.flush()
