9c70850b55f64617866c2ba75662a5957e43e6b4
= <mr.woodysimpson@gmail.com>
2023-04-26 12:38:27 +0300

    update error handler in poller, actualize api version

--- poller.py
+++ poller.py
@@ -9,24 +9,26 @@ from aiovk.longpoll import BotsLongPoll, API
 
 class Poller:
     def __init__(self, token: str, queue: asyncio.Queue):
-        TokenSession.API_VERSION = '5.103'
+        TokenSession.API_VERSION = '5.130'
         self.session = TokenSession(access_token=token)
         self.api = API(self.session)
-        self.vk_client = BotsLongPoll(self.session, group_id=os.getenv("VK_GROUP"))
+        self.vk_client = BotsLongPoll(self.session, group_id=os.getenv("VK_GROUP"), timeout=30)
         self.queue = queue
         self._task: Optional[Task] = None
 
     async def _worker(self):
-        try:
-            while True:
+        while True:
+            try:
                 async for event in self.vk_client.iter():
                     self.queue.put_nowait(event)
-        except:
-            print('Ошибка хэндлера:\n', traceback.format_exc(), flush=True)
+            except asyncio.exceptions.TimeoutError:
+                print('Ошибка хэндлера timeout error:\n', traceback.format_exc(), flush=True)
+                continue
+            except:
+                print('Ошибка хэндлера:\n', traceback.format_exc(), flush=True)
 
     async def start(self):
         self._task = asyncio.create_task(self._worker())
-
 
     async def stop(self):
         self._task.cancel()
